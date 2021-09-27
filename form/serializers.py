5279f8f0374140ae7d2f618b1f21def00a731a90
Atri Patel <Atriya.patel@gmail.com>
2021-09-27 22:22:18 +0530

    Working on Async Procession of the mail

--- form/serializers.py
+++ form/serializers.py
@@ -2,11 +2,6 @@ from rest_framework import serializers
 from form.models import FormUser
 
 
-def validate(phone):
-    if len(phone) > 12:
-        raise serializers.ValidationError("Incorrect Phone Number")
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormUser
